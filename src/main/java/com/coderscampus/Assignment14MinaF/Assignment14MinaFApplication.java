7b7f05b9a0d37e28a35c56a443203938c9156700
unknown <minakaramm1@gmail.com>
2022-04-28 12:43:30 -0400

    Setup app

---
+++ src/main/java/com/coderscampus/Assignment14MinaF/Assignment14MinaFApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Assignment14MinaF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment14MinaFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment14MinaFApplication.class, args);
+	}
+
+}
