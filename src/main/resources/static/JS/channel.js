e1799f32d0eb4a94e6e146e44e36f1cdb0e5392a
unknown <minakaramm1@gmail.com>
2022-06-20 23:07:03 -0400

    Auto refresh the page in chrome

--- src/main/resources/static/JS/channel.js
+++ src/main/resources/static/JS/channel.js
@@ -1,15 +1,24 @@
 
 var submitMessage = document.querySelector('#messageContent')
 var userId = parseInt(sessionStorage.getItem('user'), 10)
-
+//const myTimeout = setTimeout(timedRefresh, 3000);
+//console.log('xadasdasd',myTimeout)
 
 var user
 
+const myTimeout = setTimeout("location.reload(true);", 5000);
 
+function autoRefreshing() {
+	myTimeout
+}
+
+function stopRefreshing() {
+	clearTimeout(myTimeout);
+}
 
 submitMessage.addEventListener('keypress', (e) => {
-
 	if (e.key === 'Enter') {
+		setTimeout("location.reload(true);", 800);
 		var message = {
 			'messageContent': submitMessage.value,
 			user: {
@@ -18,24 +27,31 @@ submitMessage.addEventListener('keypress', (e) => {
 
 		}
 		if (message.messageContent === '') {
-			console.log("XXXXX", "message content is empty")
+			console.log('message content is empty')
 			alert('please type something')
+		} else {
+			fetch('http://localhost:8080/channels/{channelId}/{userId}', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(message)
+			}).then((response) => response.json())
+
 		}
 
 		console.log(message)
-		fetch('http://localhost:8080/channels/{channelId}/{userId}', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(message)
-		}).then((response) => response.json())
 
-		submitMessage.value = ''
+	} else {
+		stopRefreshing()
+
 	}
 
 })
-setTimeout("window.location.reload();", 1000)
-
-
+//function timedRefresh(timeoutPeriod) {
+//	setTimeout("location.reload(true);", timeoutPeriod);
+//}
+if (submitMessage.value = '') {
+	setTimeout("location.reload(true);", 800);
+}
 submitMessage.focus()
