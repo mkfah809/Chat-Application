55d81fd24355fd16c1efc2c003a2aec60496c9c7
unknown <minakaramm1@gmail.com>
2022-07-05 22:29:55 -0400

    Modified MessageService-Code Cleaned

--- src/main/java/com/coderscampus/Assignment14MinaF/service/MessageService.java
+++ src/main/java/com/coderscampus/Assignment14MinaF/service/MessageService.java
@@ -23,15 +23,7 @@ public class MessageService {
 
 	public Message saveMessage(Message message) {
 		Long channelId = null;
-
 		message.setChannel(channelService.findByChannelId(channelId));
-		channelService.findByChannelId(channelId).setMessages(channelService.findByChannelId(channelId).getMessages());
-
-		channelService.findAllChannels().add(channelService.findByChannelId(channelId));
-		channelService.findByChannelId(channelId).getMessages().add(message);
-
-		message.setUser(userService.findByUserId(message.getUser().getUserId()));
-
 		return messageRepo.save(message);
 	}
 
